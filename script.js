83b831d9121484d0b914afed4eeb9b2b17cbbc7c
Sadiafurqan <Sadiafurqan612@gmail.com>
2024-08-27 12:06:22 +0500

    Here you can see my Textanimation

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const text = "Hello,  I am Sadia ! , A Web Developer ";
+    const typingTextElement = document.getElementById('typing-text');
+    typingTextElement.textContent = text;
+});
